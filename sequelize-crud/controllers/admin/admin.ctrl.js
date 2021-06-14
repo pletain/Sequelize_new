5de4fc3e976ea10e9aaad95a869c2f7626b5892a
김태형 <thk12378905@gmail.com>
2021-06-14 10:21:13 +0000

    moment, modify, delete

--- sequelize-crud/controllers/admin/admin.ctrl.js
+++ sequelize-crud/controllers/admin/admin.ctrl.js
@@ -31,3 +31,37 @@ exports.get_products_detail = (req, res) =>{
 	}) //models. 뒤에 모델명을 넣어줘야 한다. Products.js 끝에 return하는 이름과 일치한다.
 	
 }
+
+exports.get_products_edit = ( req , res ) => {
+    //기존에 폼에 value안에 값을 셋팅하기 위해 만든다.
+    models.Products.findByPk(req.params.id).then( (product) => {
+        res.render('admin/write.html', { product : product });
+    });
+};
+
+exports.post_products_edit = ( req , res ) => {
+
+    models.Products.update(
+        {
+            name : req.body.name,
+            price : req.body.price ,
+            description : req.body.description
+        }, 
+        { 
+            where : { id: req.params.id } 
+        }
+    ).then( () => {
+        res.redirect('/admin/products/detail/' + req.params.id );
+    });
+
+	}
+
+exports.get_products_delete = ( req , res ) => {
+    models.Products.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then( () => {
+        res.redirect('/admin/products');
+    });
+};
--- sequelize-crud/controllers/admin/index.js
+++ sequelize-crud/controllers/admin/index.js
@@ -4,12 +4,20 @@ const ctrl = require('./admin.ctrl');
 
 router.get('/products', ctrl.get_products );
 
-router.get('/products/write', ctrl.get_products_write );
+router.get('/products/write', ctrl.get_products_write ); 
 
-router.post('/products/write', ctrl.post_products_write );
+router.post('/products/write', ctrl.post_products_write ); 
 
 //get으로 URL을 만들어주자
 router.get('/products/detail/:id', ctrl.get_products_detail);
+
+
+
+router.get('/products/edit/:id', ctrl.get_products_edit ); //→수정하기에서 폼이 채워져 있다.
+
+router.post('/products/edit/:id', ctrl.post_products_edit ); //→수정하기에서 폼이 채워져 있다.
+
+router.get('/products/delete/:id', ctrl.get_products_delete );
 
 
 module.exports = router;
